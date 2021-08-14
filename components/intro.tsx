7f7a2cb1421dce068b37824f8619053e093a7d4d
thor <otaviosilva2632@gmail.com>
2021-08-13 22:35:14 -0300

    feat: adjusting size of logo and title

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,11 +3,11 @@ import { BLOG_NAME } from "../lib/constants";
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 flex items-center">
+      <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:pr-8 flex items-center">
         <img
           src="/assets/svg/logo.svg"
           alt={`Logo do ${BLOG_NAME}`}
-          className="sm:w-10 sm:mr-4 md:w-20 md:mr-8 md:hidden lg:block"
+          className="sm:w-8 sm:mr-4 md:w-14 md:mr-6 lg:w-20 lg:mr-8"
         />
         {BLOG_NAME}
       </h1>
