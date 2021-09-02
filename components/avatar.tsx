a86560458e92dfd98073121c917e256e2546ebc6
Otávio Silva <otaviosilva2632@gmail.com>
2021-09-02 00:47:05 -0300

    feat(avatar.tsx): use Link for redirect to authors page

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,14 +1,22 @@
+import Link from "next/link";
+import { Author } from "../types/author";
+
 type Props = {
-  name: string;
-  picture: string;
+  author: Author;
 };
 
-const Avatar = ({ name, picture }: Props) => {
+const Avatar = ({ author }: Props) => {
   return (
-    <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
-      <div className="text-xl font-bold">{name}</div>
-    </div>
+    <Link as={`/authors/${author.user}`} href="/authors/[slug]">
+      <a className="hover:underline flex items-center">
+        <img
+          src={author.picture}
+          className="w-12 h-12 rounded-full mr-4"
+          alt={author.name}
+        />
+        <div className="text-xl font-bold">{author.name}</div>
+      </a>
+    </Link>
   );
 };
 
